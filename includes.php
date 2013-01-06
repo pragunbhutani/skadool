2a5fd8ee3f50ce4aee7382dbe104a92c191d2d03
Pragun Bhutani <pragun06@gmail.com>
2013-01-07 04:21:41 +0530

    Added db connection

---
+++ includes.php
@@ -0,0 +1,15 @@
+<?php
+
+$username = "root";
+$password = "";
+$hostname = "localhost"; 
+
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password) 
+	or die("Unable to connect to MySQL.");
+
+//select a database to work with
+$selected = mysql_select_db("skadool",$dbhandle) 
+  or die("Could not select skadool.");
+
+?>
