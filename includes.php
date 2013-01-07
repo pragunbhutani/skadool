a42ba949b77bc6c8fa3829005a5f747cc966d01a
Pragun Bhutani <pragun06@gmail.com>
2013-01-08 03:04:10 +0530

    Updated and working with AppFog

--- includes.php
+++ includes.php
@@ -1,16 +1,21 @@
 <?php
 
-$username = "root";
-$password = "";
-$hostname = "localhost"; 
+$services_json = json_decode(getenv("VCAP_SERVICES"),true);
+$mysql_config = $services_json["mysql-5.1"][0]["credentials"];
+
+$username = $mysql_config["username"];
+$password = $mysql_config["password"];
+$hostname = $mysql_config["hostname"];
+$port = $mysql_config["port"];
+$db = $mysql_config["name"];
 
 //connection to the database
-$dbhandle = mysql_connect($hostname, $username, $password) 
+$link = mysql_connect("$hostname:$port", $username, $password) 
 	or die("Unable to connect to MySQL.");
 
 //select a database to work with
-$selected = mysql_select_db("skadool",$dbhandle) 
-  or die("Could not select skadool.");
+$selected = mysql_select_db($db, $link) 
+  or die("Could not select the database.");
 
 
 ?>
