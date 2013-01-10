119418f972537dc2cd8d9a1a90d0ddf3c29c9210
Pragun Bhutani <pragun06@gmail.com>
2013-01-11 02:59:35 +0530

    Fixing SQL

--- createTableVisits.php
+++ createTableVisits.php
@@ -4,14 +4,13 @@ require_once 'includes.php';
 
 $sql = "CREATE TABLE visits 
 (
-visitID int NOT NULL AUTO_INCREMENT, 
-PRIMARY KEY(contactID),
+visitID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 contactName varchar(40),
 contactNumber varchar(20),
 status varchar(20) DEFAULT 'Uscheduled',
-)";
+);";
 
-mysql_query($sql, $conn) or die("Could not create table Visits.");
+mysql_query($sql, $conn) or die("Could not create table Visits. " . mysql_error());
 
 
 ?>
