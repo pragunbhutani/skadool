ea28c88da3f508b0c0091fc7094b4f2b1fa11fea
Pragun Bhutani <pragun06@gmail.com>
2013-01-21 05:21:18 +0530

    Removed unnecessary files

--- insertValues.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'includes.php';
-
-$insert = "INSERT INTO `visits` (`visitID`, `contactName`, `contactNumber`, `status`) VALUES (NULL, '" . $_GET['name'] . "', '" . $_GET['number'] . "', 'Unscheduled')";
-
-mysql_query($insert, $conn) or die("Could not insert values into the table. " . mysql_error());
-
-?>
--- updateValues.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'includes.php';
-
-$update = "UPDATE visits SET contactNumber = '18565751196' WHERE visitID = 2";
-
-mysql_query($update, $conn) or die(mysql_error());
-
-?>
