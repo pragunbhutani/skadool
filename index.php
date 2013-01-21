1e7768e5ac46e2d6650111d6d407a1b5169515a3
Pragun Bhutani <pragun06@gmail.com>
2013-01-21 14:35:11 +0530

    Dressed up with Bootstrap

--- index.php
+++ index.php
@@ -4,47 +4,76 @@ require_once 'includes.php';
 
 ?>
 
+<!DOCTYPE html>
+
 <html>
 
-<?php
+<head>
 
-echo '<h3>List of Visits</h3>';
+	<title>Skadool</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
 
-$result = mysql_query('SELECT * FROM visits');
+</head>
 
-if(mysql_num_rows($result))	{
-	echo '<table cellpadding="10" cellspacing="10" class="db-table">';
-	echo '<tr><th>Visit ID</th><th>Contact Name</th><th>Contact Number</th><th>Status</th></tr>';
+<body>
 
-	while($row = mysql_fetch_row($result))	{
-		echo '<tr>';
+<div class="container">
 
-		foreach($row as $cell)	{
-			echo '<td>' . $cell . '</td>';
+	<div class="page-header">
+		<h1>Skadool <small>Flexible scheduling powered by plivo.</small></h1>
+	</div>	
+
+	<?php
+
+	echo '<h3>List of Visits</h3>';
+
+	$result = mysql_query('SELECT * FROM visits');
+
+	if(mysql_num_rows($result))	{
+		echo '<table cellpadding="10" cellspacing="10" class="table table-hover">';
+		echo '<tr><th>Visit ID</th><th>Contact Name</th><th>Contact Number</th><th>Status</th></tr>';
+
+		while($row = mysql_fetch_row($result))	{
+			echo '<tr>';
+
+			foreach($row as $cell)	{
+				echo '<td>' . $cell . '</td>';
+			}
+
+			echo '</tr>';
+
 		}
 
-		echo '</tr>';
+		echo '</table><br />';
+
+		?>
+
+		<div class="span4 offset4">
+
+			<form name="input" action="sendTexts.php" method="get">
+				<div class="input-append">
+					<input class="span3" id="appendedInputButton" type="text" name="countTexts" placeholder="Number of clients to text" />
+  					<button class="btn btn-primary" type="submit">Go!</button>
+				</div>		
+			</form>
+
+		</div>
+
+	<?php
 
 	}
 
-	echo '</table><br />';
+	else 	{
+		echo '<p class="lead">No visits in record.</p>';
+	}
 
 	?>
 
-	<form name="input" action="sendTexts.php" method="get">
-		Number of clients to text : <input type="text" name="countTexts" />
-		<input type="submit" value="Submit" />
-	</form>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
 
-<?php
+</div>
 
-}
-
-else 	{
-	echo '<p>No visits in record.</p>';
-}
-
-?>
-
+</body>
 
 </html>
