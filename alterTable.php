671522a995d443a480425c4dd678fb9932e39f81
Pragun Bhutani <pragun06@gmail.com>
2013-01-22 00:22:45 +0530

    Update alterTable.php

--- alterTable.php
+++ alterTable.php
@@ -2,7 +2,7 @@
 
 	require_once "includes.php"
 
-	$query = "ALTER TABLE visits ADD updateTime DATETIME NULL";
+	$query = "ALTER TABLE visits ADD updatedTime BIGINT";
 
 	mysql_query($query) or die('Could not alter table : ' . mysql_error());
 
